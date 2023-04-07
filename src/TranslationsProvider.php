6920b40faa542d6c0d99004a34e0a7401e62cfb4
Joshua Thijssen <jthijssen@noxlogic.nl>
2023-04-07 15:52:07 +0200

    initial checkin

---
+++ src/TranslationsProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MinVWS\Translations\Laravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class TranslationsProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+    }
+}
---
+++ tests/HelloWorldTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MinVWS\Laravel\Translation\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class HelloWorldTest extends TestCase
+{
+    public function testHelloWorld(): void
+    {
+        $this->assertTrue(true);
+    }
+}
